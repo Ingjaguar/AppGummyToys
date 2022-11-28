bc52780362e4d31feb9c8bf0b819635b9f3e3304
Ingjaguar <ing.jaguarin@gmail.com>
2022-11-27 21:09:57 -0500

    Add build

---
+++ front/build/precache-manifest.7f257769cfb90915d41fc6521c8a9c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9768ec7f12abfb1cdb",
+    "url": "/static/css/main.f653d0a8.chunk.css"
+  },
+  {
+    "revision": "6d9768ec7f12abfb1cdb",
+    "url": "/static/js/main.99b8a857.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a457fa7b11802ed55e6",
+    "url": "/static/css/2.161db36f.chunk.css"
+  },
+  {
+    "revision": "6a457fa7b11802ed55e6",
+    "url": "/static/js/2.a2f78239.chunk.js"
+  },
+  {
+    "revision": "45df98a1220b464062877900908b6573",
+    "url": "/index.html"
+  }
+];
